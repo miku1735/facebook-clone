2c20fae187f75c7d52ea447992dc1d201384ac2b
Mayank <Mayank13@ibm.com>
2021-04-03 13:00:54 +0530

    Updates

---
+++ precache-manifest.8fa57edf4dd0e2d74bdd3fc47296b390.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067ed83075c44718b123aa24477bdb2e",
+    "url": "/facebookclone/index.html"
+  },
+  {
+    "revision": "a3101e8d9b7031d8d47f",
+    "url": "/facebookclone/static/css/main.373e0748.chunk.css"
+  },
+  {
+    "revision": "b7860c1285fff36e0dc6",
+    "url": "/facebookclone/static/js/2.d277b4dd.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/facebookclone/static/js/2.d277b4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3101e8d9b7031d8d47f",
+    "url": "/facebookclone/static/js/main.833f4d22.chunk.js"
+  },
+  {
+    "revision": "50eb3e6396e4631b0891",
+    "url": "/facebookclone/static/js/runtime-main.318e83d2.js"
+  }
+]);
